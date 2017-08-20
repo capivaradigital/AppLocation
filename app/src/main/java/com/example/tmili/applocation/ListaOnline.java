ee68ebf466d243cf5ded53bb1fd374f37ca8b0de
tmilitino <tmilitino.tm@gmail.com>
2017-08-20 16:22:28 -0300

    Introdução das lista dos sites

---
+++ app/src/main/java/com/example/tmili/applocation/ListaOnline.java
@@ -0,0 +1,13 @@
+package com.example.tmili.applocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaOnline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_online);
+    }
+}
